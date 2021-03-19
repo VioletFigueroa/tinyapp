c13bf143c9c3f421ce5307d3d6ca332e8f973264
VioletFigueroa <Permafry_42@hotmail.com>
2021-03-19 04:04:42 +0000

    created username cookie when logged in

--- express_server.js
+++ express_server.js
@@ -4,8 +4,9 @@ const PORT = 8080;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+
+const cookieParser = require("cookie-parser");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -49,11 +50,13 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.post("/login", (req, res) => {
   const name = req.body['username'];
-  res.cookie(username, name);
+  res.cookie("username", name);
   res.redirect(`/urls/`);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
