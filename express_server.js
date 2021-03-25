2dfac7ba8a1a36fe38c48da9834c703ef5ebd521
VioletFigueroa <Permafry_42@hotmail.com>
2021-03-25 04:34:19 +0000

    Fixed logging in with unknown email address bug, and refactored and reorganized code to be cleaner with clearer comments

--- express_server.js
+++ express_server.js
@@ -5,11 +5,14 @@ const cookieSession = require('cookie-session');
 const express = require("express");
 const helpers = require('./helpers.js');
 const lodash = require("lodash");
+
 // Express Settings;
 const app = express();
 const PORT = 8080;
+
 // Bodyparser Settings;
 app.use(bodyParser.urlencoded({extended: true}));
+
 // CookieSession Settings;
 app.use(cookieSession({
   name: 'session',
@@ -17,8 +20,10 @@ app.use(cookieSession({
   //Cookie Options
   maxAge: 24 * 60 * 60 * 1000 //24 hours
 }));
+
 // Sets express view engine as ejs files;
 app.set("view engine", "ejs");
+
 // Sample users database with test user accounts;
 const users = {
   "fz7rsp6sc7wky8qohzsfeiw0qhachayizw18kkunff": {
@@ -32,158 +37,275 @@ const users = {
     "password": "$2b$10$GMXdN0T6/kG9QTLOHBFrv.cjFdr2v4JWK.Xd6GWZEp8B55LyYuZZC"
   }
 };
-// Sample URL Database with test shortened link string, longURL with website link info, and userID to determine which user created the shortened URL;
+
+// Sample URL Database
+
 const urlDatabase = {
-  b6UTxQ: { longURL: "https://youtu.be/GNZMCS2dbag", userID: "fz7rsp6sc7wky8qohzsfeiw0qhachayizw18kkunff" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "3oqdszqj6vi2u7mnmqa9qr5z3tf08sz1oboq4kgv6g" }
+  b6UTxQ: {
+    longURL: "https://youtu.be/GNZMCS2dbag",
+    userID: "fz7rsp6sc7wky8qohzsfeiw0qhachayizw18kkunff"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "3oqdszqj6vi2u7mnmqa9qr5z3tf08sz1oboq4kgv6g"
+  }
 };
+
 // Tinyapp listening message so server owner knows which port user should access;
 app.listen(PORT, () => {
   console.log(`Tinyapp listening on port ${PORT}!`);
 });
-// Homepage link, which redirects to /login if not logged in or /urls if logged in;
+
+// Homepage link
 app.get("/", (req, res) => {
   const userID = req.session.userID;
-  if (!userID) return res.redirect("/login");
+  if (!userID) {
+    return res.redirect("/login");
+    //Redirects to /login if not logged in;
+  }
   return res.redirect("/urls");
+  // Redirects to /urls if logged in;
 });
-// Shows list of user's urls if logged in, or sends to /login with error message "Please log in" in the header of login page if logged out;
+
 app.get("/urls", (req, res) => {
   const userID = req.session.userID;
   if (!userID) return res.redirect("/login");
+  // Sends to /login with error message "Please log in" in the header of login page if logged out;
   const templateVars = { user: users[userID], urls: helpers.urlsForUser(userID, urlDatabase) };
   return res.render("urls_index", templateVars);
+  // Shows list of user's urls if logged in;
 });
-// Shows form to submit new urls to shorten if logged in, or sends to /login with error message "Please log in" in the header of login page if logged out;
+
 app.get("/urls/new", (req, res) => {
   const userID = req.session.userID;
-  if (!userID) return res.redirect("/login");
+  if (!userID) {
+    return res.redirect("/login");
+  }
+  // Sends to /login with error message "Please log in" in the header of login page if logged out;
   const templateVars = { user: users[userID] };
   return res.render("urls_new", templateVars);
+  // Shows form to submit new urls to shorten if logged in
 });
-// Shows a page with the link submitted and a shortened link version if logged in;
-// Redirects to /login with message "Please Log in" in form if user isn't logged in;
-// Sends an error if user doesn't didn't create the link;
-// Sends Error 404 if shortened URL given doesn't exist on the server;
+
 app.get("/urls/:id", (req, res) => {
-  const shortURL = req.params.id;
   const userID = req.session.userID;
-  if (!userID) return res.redirect("/login");
-  if (userID !== urlDatabase[shortURL]["userID"]) return res.status(401).send('Please login as the owner of this link to access it.');
-  if (lodash.isUndefined(helpers.urlsForUser(userID, urlDatabase))) return res.status(404).send(`Please try another link. This link was not found. \n Here's another link instead to make up for it ;P - https://youtu.be/GNZMCS2dbag ;`);
+  if (!userID) {
+    return res.redirect("/login");
+    // Redirects to /login with error message "Please Log in" in form if user isn't logged in;
+  }
+  const shortURL = req.params.id;
+  if (userID !== urlDatabase[shortURL]["userID"]) {
+    return res.status(401).send(
+      'Please login as the owner of this link to access it;'
+    );
+    // Sends an error if user doesn't didn't create the link;
+  }
+  if (lodash.isUndefined(helpers.urlsForUser(userID, urlDatabase))) {
+    return res.status(404).send(
+      `Please try another link. This link was not found.
+    \n Here's another link instead to make up for it ;P
+    \n https://youtu.be/GNZMCS2dbag ;`
+    );
+    // Sends Error 404 if shortened URL given doesn't exist on the server;
+  }
   const user = users[userID];
   const longURL = urlDatabase[shortURL]["longURL"];
   const templateVars = { user, shortURL, longURL };
   return res.render("urls_show", templateVars);
+  // Shows a page with the link submitted and a shortened link version if logged in;
 });
-// Redirects to original url if it exists for this short url;
-// Sends Error 404 if shortened URL given doesn't exist on the server;
+
 app.get("/u/:id", (req, res) => {
-  if (!urlDatabase[req.params.id]["longURL"]) {
-    return res.status(404).send(`Please try another link. This link was not found. \n Here's another link instead to make up for it ;P - https://youtu.be/GNZMCS2dbag ;`);
+  const urlID = req.params.id;
+  if (!urlDatabase[urlID]["longURL"]) {
+    return res.status(404).send(
+      `Please try another link. This link was not found.
+    \n Here's another link instead to make up for it ;P
+    \n https://youtu.be/GNZMCS2dbag ;`
+    );
+    // Sends Error 404 if shortened URL given doesn't exist on the server;
   }
-  const longURL = urlDatabase[req.params.id]["longURL"];
+  const longURL = urlDatabase[urlID]["longURL"];
   return res.redirect(longURL);
+  // Redirects to original url if it exists for this short url;
 });
-// Creates a new urlDatabase entry with given URL, a generated short URL, and the userID of who created the shortURL, if logged in;
-// Redirects to a page with shortened link version of submitted link if logged in;
-// Redirects to /login with message "Please Log in" in form if user isn't logged in;
-// Returns an error if longURL was not submitted;
+
 app.post("/urls/", (req, res) => {
   const userID = req.session.userID;
-  if (!userID) return res.redirect("/login");
-  if (!req.body.longURL) return res.status(400).send('Please enter a link');
-  const shortURL = helpers.generateRandomString(6, urlDatabase);
+  if (!userID) {
+    return res.redirect("/login");
+  // Redirects to /login with message "Please Log in" in form if user isn't logged in;
+  }
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = {longURL, userID};
+  if (!longURL) {
+    return res.status(400).send(
+      'Please enter a link;'
+    );
+    // Returns an error if longURL was not submitted;
+  }
+  const shortURL = helpers.generateRandomString(6, urlDatabase); // A new generated short URL
+  urlDatabase[shortURL] = { longURL, userID }; // Creates a new urlDatabase entry with given URL
+  // Includes the userID of who created the shortURL
   return res.redirect(`/urls/${shortURL}`);
+  // Redirects to a page with shortened link version of submitted link if logged in;
 });
-// Updates a urlDatabase entry with given URL amd the previous shortURL, if logged in;
-// Redirects to user urls following edit;
-// Redirects to /login with message "Please Log in" in form if user isn't logged in;
-// Returns an error if longURL was not submitted;
+
 app.post("/urls/:id", (req, res) => {
+  // Updates a urlDatabase entry with given URL amd the previous shortURL, if logged in;
   const userID = req.session.userID;
-  if (!userID) return res.redirect("/login");
-  if (userID !== urlDatabase[req.params.id]["userID"]) return res.status(401).send('Please login as this links owner to access this link');
-  urlDatabase[req.params.id]["longURL"] = req.body["longURL"];
+  if (!userID) {
+    return res.redirect("/login");
+  // Redirects to /login with message "Please Log in" in form if user isn't logged in;
+  }
+  const urlID = req.params.id;
+  if (userID !== urlDatabase[urlID]["userID"]) {
+    return res.status(401).send(
+      'Please login as this links owner to access this link;'
+    );
+    // Returns an error if longURL was not submitted;
+  }
+  urlDatabase[urlID]["longURL"] = req.body["longURL"];
   return res.redirect("/urls/");
+  // Redirects to user urls following edit;
 });
-// Deletes a urlDatabase entry with shortURL, if logged in;
-// Redirects to /urls following removal;
-// Redirects to /login with message "Please Log in" in form if user isn't logged in;
-// Returns an error if shorturl requested for deletion wasn't created by this user;
+
 app.post("/urls/:id/delete", (req, res) => {
-  if (!req.session.userID) return res.redirect("/login");
-  if (req.session.userID !== urlDatabase[req.params.id]["userID"]) return res.status(403).send('Please login as the link owner to delete this link');
-  if (req.params.id && urlDatabase[req.params.id]) delete urlDatabase[req.params.id];
+  const userID = req.session.userID;
+  if (!userID) {
+    return res.redirect("/login");
+    // Redirects to /login with message "Please Log in" in form if user isn't logged in;
+  }
+  const urlID = req.params.id;
+  if (userID !== urlDatabase[urlID]["userID"]) {
+    return res.status(403).send(
+      'Please login as the link owner to delete this link;'
+    );
+    // Returns an error if shorturl requested for deletion wasn't created by this user;
+  }
+  if (urlDatabase[urlID]) {
+    return delete urlDatabase[urlID];
+    // Deletes a urlDatabase entry with shortURL, if logged in;
+  }
   return res.redirect("/urls");
+  // Redirects to /urls following removal;
 });
-// Redirects to /urls if user is logged in;
-// Renders login form if user is not logged in;
+
 app.get("/login", (req, res) => {
+  if (req.session.userID) {
+    return res.redirect("/urls");
+    // Redirects to /urls if user is logged in;
+  }
   const templateVars = {user: null};
-  if (req.session.userID) return res.redirect("/urls");
   return res.render("login", templateVars);
+  // Renders login form if user is not logged in;
 });
-// Redirects to /urls if user is logged in;
-// Renders Register form if user is not logged in;
+
 app.get("/register", (req, res) => {
-  let templateVars = {user: null};
-  if (req.session.userID) return res.redirect("/urls");
+  const templateVars = {user: null};
+  if (req.session.userID) {
+    return res.redirect("/urls");
+    // Redirects to /urls if user is logged in;
+  }
   return res.render("register", templateVars);
+  // Renders Register form if user is not logged in;
 });
-// Sets userID Cookie and Redirects to /urls;
-// Returns error messages if email or password is empty;
-// Returns error messages if given email isn't found;
-// Returns error messages if password and email don't match stored info;
+
 app.post("/login", (req, res) => {
   const formEmail = req.body["email"];
-  if (!formEmail) return res.status(400).send('Please enter an email');
+  if (!formEmail) {
+    return res.status(400).send(
+      'Please enter an email;'
+    );
+    // Returns error messages if email or password is empty;
+  }
   const formPassword = req.body["password"];
-  if (!formPassword) return res.status(400).send('Please enter a password');
+  if (!formPassword) {
+    return res.status(400).send(
+      'Please enter a password;'
+    );
+    // Returns error messages if password is empty;
+  }
   const userEmail =  helpers.propertySearch(users, "email", formEmail);
-  if (!userEmail) return res.status(400).send('Please enter an email registered with an account');
+  if (lodash.isEmpty(userEmail)) {
+    return res.status(403).send('Please enter an email registered with an account;');
+    // Returns error messages if given email isn't found;
+  }
   const userID = Object.keys(userEmail)[0];
-  if (!bcrypt.compareSync(formPassword, users[userID]["password"])) return res.status(400).send('Please enter a correct Email and Password');
+  console.log("UserID: ", userID);
+  console.log("Users: ", users);
+  if (!bcrypt.compareSync(formPassword, users[userID]["password"])) {
+    return res.status(400).send(
+      'Please enter a correct Email and Password;'
+    );
+    // Returns error messages if password and email don't match stored info;
+  }
   req.session.userID = userID;
+  // Sets userID Cookie
   return res.redirect("/urls");
+  //Redirects to /urls;
 });
-// Creates a new user and generates a new ID;
-// Hashes password with bcrypt;
-// Registers user info into users database, storing hashed password and NOT plaintext password;
-// Sets userID Cookie and Redirects to /urls;
-// Returns error messages if email or password is empty;
+
 // Returns error messages if given email already exists;
 app.post("/register", (req, res) => {
   const formEmail = req.body["email"];
-  if (!formEmail) return res.status(400).send('Please enter an email');
+  if (!formEmail) {
+    return res.status(400).send(
+      'Please enter an email'
+    );
+  // Returns error messages if email is empty;
+  }
   const formPassword = req.body["password"];
-  if (!formPassword) return res.status(400).send('Please enter a password');
+  if (!formPassword) {
+    return res.status(400).send(
+      'Please enter a password'
+    );
+    // Returns error messages if password is empty;
+  }
   const userEmail = helpers.getUserByEmail(formEmail, users);
-  if (userEmail) return res.status(400).send('Please enter an Email not already in use on this website');
+  if (userEmail) {
+    return res.status(400).send(
+      'Please enter an Email not already in use on this website'
+    );
+  }
   const userID = helpers.generateRandomString(42, users);
+  //Generates a new ID;
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  // Hashes password with bcrypt;
   users[userID] = {};
+  // Creates a new user;
   users[userID]["id"] = userID;
   users[userID]["email"] = req.body["email"];
   users[userID]["password"] = hashedPassword;
+  // Registers user info into users database, storing hashed password and NOT plaintext password;
   req.session.userID = userID;
+  // Sets userID Cookie
   return res.redirect("/urls/");
+  //Redirects to /urls;
 });
-// Deletes user session cookie;
-// Redirects to /urls page;
+
 app.post("/logout", (req, res) => {
   delete req.session.userID;
+  // Deletes user session cookie;
   return res.redirect("/urls");
+  // Redirects to /urls page;
 });
-// Deletes a urlDatabase entry with shortURL, if logged in;
-// Redirects to /urls following removal;
-// Redirects to /login with message "Please Log in" in form if user isn't logged in;
-// Returns an error if shorturl requested for deletion wasn't created by this user;
+
 app.get("/urls/:id/delete", (req, res) => {
-  if (!req.session.userID) return res.redirect("/login");
-  if (req.session.userID !== urlDatabase[req.params.id]["userID"]) return res.status(403).send('Please login as the link owner to delete this link');
-  if (req.params.id && urlDatabase[req.params.id]) delete urlDatabase[req.params.id];
+  const userID = req.session.userID;
+  if (!userID) {
+    return res.redirect("/login");
+    // Redirects to /login with message "Please Log in" in form if user isn't logged in;
+  }
+  const urlID = req.params.id;
+  if (userID !== urlDatabase[urlID]["userID"]) {
+    return res.status(403).send('Please login as the link owner to delete this link');
+    // Returns an error if shorturl requested for deletion wasn't created by this user;
+  }
+  
+  if (urlDatabase[urlID]) {
+    delete urlDatabase[urlID];
+  // Deletes a urlDatabase entry with shortURL, if logged in;
+  }
   return res.redirect("/urls");
+  // Redirects to /urls following removal;
 });
--- helpers.js
+++ helpers.js
@@ -1,36 +1,67 @@
 // Libraries;
+
 const lodash = require("lodash");
-// Takes an object of objects database, a key, and an expected value for that key value pair;
-// Returns the objects in object database that contains the key value pair in that object;
-// Returns empty object if no matches found in objects;
+
+// Helper Functions:
+
 const propertySearch = (object, key, value) => {
+  // Takes an object of objects database, a key, and an expected value for that key value pair;
   let output = {};
+  // Returns empty object if no matches found in objects;
   for (const id of Object.keys(object)) if (object[id][key] === value) output[id] = object[id];
   return output;
-};
-// Takes in a UserID and an object of objects database;
-// Returns the objects in object database that contains the UserID as a property is found in that object;
-// Returns undefined if no matches found in objects;
-const urlsForUser = (userID, database) => lodash.isEmpty(propertySearch(database, "userID", userID)) ? undefined : propertySearch(database, "userID", userID);
-// Takes in a email and an object of objects database;
-// Returns the objects in object database that contains the email as a property is found in that object;
-// Returns undefined if no matches found in objects;
-const getUserByEmail = (email, database) => lodash.isEmpty(propertySearch(database, "email", email)) ? undefined : propertySearch(database, "email", email)[Object.keys(database)[0]];
-//Returns a random (lowercase only) a-z or 0-9 char;
-const generateRandomChar = () => {
-  const chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'];
-  return chars[Math.floor(Math.random() * chars.length)];
-};
-// Takes in a desired string length, an object of objects database the generated string will be used for;
-// If length is 0 or less, an empty string is returned;
-// Runs generateRandomChar() the given length amount of times, storing each return into a string output;
-// If the generated string matches a string already present in database, recursion is used to generate a different string;
-// Once a string generated is not already present in database, it returns that random string;
-const generateRandomString = (length, database) => {
-  let randomString = '';
-  for (let i = 0; i < length; i++) randomString += generateRandomChar();
-  if (!lodash.isEmpty(propertySearch(database, "shortURL", randomString))) return generateRandomString(length);
-  return randomString;
+  // Returns the objects in object database that contains the key value pair in that object;
 };
 
-module.exports = { propertySearch, urlsForUser, generateRandomString, getUserByEmail };
+// Returns undefined if no matches found in objects;
+const urlsForUser = (userID, database) => {
+  // Takes in a UserID and an object of objects database;
+  if (lodash.isEmpty(propertySearch(database, "userID", userID))) {
+    return undefined;
+  }
+  return propertySearch(database, "userID", userID);
+  // Returns the objects in object database that contains the UserID as a property is found in that object;
+};
+
+const getUserByEmail = (email, database) => {
+  // Takes in a email and an object of objects database;
+  if (lodash.isEmpty(propertySearch(database, "email", email))) {
+    return undefined;
+    // Returns undefined if no matches found in objects;
+  }
+  return propertySearch(database, "email", email)[Object.keys(database)[0]];
+  // Returns the objects in object database that contains the email as a property is found in that object;
+};
+
+const generateRandomChar = () => {
+  const chars = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    '0','1','2','3','4','5','6','7','8','9'
+  ];
+  return chars[Math.floor(Math.random() * chars.length)];
+  //Returns a random (lowercase only) a-z or 0-9 char;
+};
+
+const generateRandomString = (length, database) => {
+  // Takes in a desired string length, an object of objects database the generated string will be used for;
+  let randomString = ''; // If length is 0 or less, an empty string is returned;
+  for (let i = 0; i < length; i++) {
+    randomString += generateRandomChar();
+  // Runs generateRandomChar() the given length amount of times, storing each return into a string output;
+  }
+  if (!lodash.isEmpty(propertySearch(database, "shortURL", randomString))) {
+    return generateRandomString(length);
+    // If the generated string matches a string already present in database, recursion is used to generate a different string;
+  }
+  return randomString;
+  // Once a string generated is not already present in database, it returns that random string;
+};
+
+// Exports:
+
+module.exports = {
+  propertySearch,
+  urlsForUser,
+  generateRandomString,
+  getUserByEmail
+};
